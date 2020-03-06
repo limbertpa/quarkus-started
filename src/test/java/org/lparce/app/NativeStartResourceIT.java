36815b09d3f4a1234f2d9f14ab1c5787e17ad3ca
lparce <limbertarceperez@gmail.com>
2020-03-06 08:37:09 -0400

    setting proyect

---
+++ src/test/java/org/lparce/app/NativeStartResourceIT.java
@@ -0,0 +1,9 @@
+package org.lparce.app;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeStartResourceIT extends StartResourceTest {
+
+    // Execute the same tests but in native mode.
+}
