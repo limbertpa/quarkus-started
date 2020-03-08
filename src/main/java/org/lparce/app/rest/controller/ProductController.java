e317bab149294c3c3a0d82c344de396c68052002
limbertp <limbertarceperez@gmail.com>
2020-03-07 21:29:18 -0400

    add git ignore

---
+++ src/main/java/org/lparce/app/rest/controller/ProductController.java
@@ -0,0 +1,27 @@
+package org.lparce.app.rest.controller;
+
+
+import org.lparce.app.core.entity.Product;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/products")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProductController {
+
+    @GET
+    public List<Product> list() {
+        return Product.listAll();
+    }
+
+    @Transactional
+    @POST
+    public Product add (Product product){
+        product.persist();
+        return product;
+    }
+}
